1cdfbe7987199fed5c091a75a84cbead5b4e7882
KruglikovIgor <ikruglikov@list.ru>
2024-07-28 22:29:10 +0300

    Add files via upload

---
+++ CSDesktopApp.WinForm,LaboratoryWork2-3-ToolStrip/CSDesktopApp.WinForm,LaboratoryWork2-3-ToolStrip/CSDesktopApp.WinForm,LaboratoryWork2-4-StatusStrip/CSDesktopApp.WinForm,LaboratoryWork2-4-StatusStrip/ParentForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSDesktopApp.WinForm_LaboratoryWork1_4_MdiApplication
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+            spWin.Text = "Окна расположены каскадом";
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+            spWin.Text = "Окна расположены плиткой";
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+            newChild.MdiParent = this;
+            newChild.Show();
+           
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " + ++openDocuments;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    spWin.Text = "Окна расположены каскадом";
+                    break;
+                case "Title":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    spWin.Text = "Окна расположены плиткой";
+                    break;
+            }
+
+            }
+        }
+}
