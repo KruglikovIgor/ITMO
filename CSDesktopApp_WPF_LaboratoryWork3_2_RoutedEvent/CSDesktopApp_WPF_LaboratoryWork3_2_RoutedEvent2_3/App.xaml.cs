18ae6d0ea9b9231a7a39a7dcd53bd70228a2c721
KruglikovIgor <ikruglikov@list.ru>
2024-07-30 22:22:22 +0300

    Add files via upload

---
+++ CSDesktopApp_WPF_LaboratoryWork3_2_RoutedEvent/CSDesktopApp_WPF_LaboratoryWork3_2_RoutedEvent2_3/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CSDesktopApp_WPF_LaboratoryWork3_2_RoutedEvent2_3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
