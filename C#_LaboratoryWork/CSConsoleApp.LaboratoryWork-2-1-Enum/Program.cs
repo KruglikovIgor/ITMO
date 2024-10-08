c40cab8072f2fa9e5644fd69587c03817b5bf55a
KruglikovIgor <ikruglikov@list.ru>
2024-10-08 23:28:19 +0300

    Add files via upload

---
+++ C#_LaboratoryWork/CSConsoleApp.LaboratoryWork-2-1-Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CSConsoleApp.LaboratoryWork_2_1_Enum
+{
+    public enum AccountType { Checking, Deposit }
+    public class Program
+    {
+        public static void Main()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#_LaboratoryWork/CSConsoleApp.LaboratoryWork-2-2-StructType/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CSConsoleApp.LaboratoryWork_2_2_StructType
+{
+    public enum AccountType { Checking, Deposit }
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            BankAccount goldAccount;
+
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            Console.Write("Введите номер счета: ");
+            goldAccount.accNo = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Номер счета: {0}", goldAccount.accNo);
+            Console.WriteLine("Проверка счета: {0}", goldAccount.accType);
+            Console.WriteLine("Баланс счета: ${0}", goldAccount.accBal);
+            Console.ReadLine();
+        }
+    }
+}
